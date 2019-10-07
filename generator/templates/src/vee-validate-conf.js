7275509ae5ec8ffcd8200bfdd6adbe8cd4d59a3c
Juan Cristobal Pazos <juancri@modyo.com>
2019-10-07 18:00:54 -0300

    minor lint fixes

--- generator/templates/src/vee-validate-conf.js
+++ generator/templates/src/vee-validate-conf.js
@@ -17,16 +17,16 @@ import {
 import i18n from './i18n'
 
 configure({
-  defaultMessage: (_, values) => i18n.t(`validations.${values._rule_}`, values),
+  defaultMessage: ( _, values ) => i18n.t( `validations.${values._rule_}`, values ),
 })
 
-extend('required', required)
-extend('email', email)
-extend('numeric', numeric)
-extend('min_value', min_value)
-extend('max_value', max_value)
+extend( 'required', required )
+extend( 'email', email )
+extend( 'numeric', numeric )
+extend( 'min_value', min_value )
+extend( 'max_value', max_value )
 
-extend('rut', {
-  validate: value => rutUtilities.validate(value),
+extend( 'rut', {
+  validate: ( value ) => rutUtilities.validate( value ),
   message: 'El {_field_} ingresado no es valido',
 })

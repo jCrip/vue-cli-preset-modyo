358ab75c9d8b8df0d87c912dcb94a0daa1470bfc
Juan Cristobal Pazos <juancri@modyo.com>
2019-10-09 17:32:44 -0300

    comment configurations

--- generator/templates/src/i18n.js
+++ generator/templates/src/i18n.js
@@ -1,5 +1,11 @@
+/* Config file for i18n plugin */
+
 import Vue from 'vue'
 import VueI18n from 'vue-i18n'
+
+// The default language is spanish.
+// If you add another language to your project be sure to:
+// STEP 1: import the validations messages from vee-validate.
 import es from 'vee-validate/dist/locale/es.json'
 
 Vue.use( VueI18n )
@@ -14,10 +20,14 @@ function loadLocaleMessages() {
       messages[locale] = locales( key )
     }
   })
+
+  // STEP 2: add the validation messages to the i18n plugin.
+  // Copy the code below for every new language you add.
   messages.es = {
     ...messages.es,
     validations: es.messages,
   }
+
 
   return messages
 }

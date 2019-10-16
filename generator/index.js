424374ab474843428300ade4eececf5e00d819fc
Juan Cristobal Pazos <juancri@modyo.com>
2019-10-16 11:17:44 -0300

    ask for widget name

--- generator/index.js
+++ generator/index.js
@@ -4,8 +4,9 @@
  * @param {any} options
  */
 
+const slugify = require( '../slugify' )
 
-module.exports = ( api ) => {
+module.exports = ( api, options ) => {
   try {
     /*
      * extend packages
@@ -13,6 +14,7 @@ module.exports = ( api ) => {
 
     const pkg = {
       dependencies: {
+        // TODO: uncomment when ready
         // '@modyo/commons': '1.0.0',
         // '@modyo/vue-cli-plugin-cmds': '1.0.0'
       },
@@ -31,6 +33,18 @@ module.exports = ( api ) => {
      */
 
     api.render( './templates' )
+
+    api.onCreateComplete(() => {
+      const files = []
+      files.push( api.resolve( 'public/index.html' ))
+      files.push( api.resolve( 'src/main.js' ))
+      const fs = require( 'fs' )
+      files.forEach(( file ) => {
+        let content = fs.readFileSync( file, { encoding: 'utf-8' })
+        content = content.replace( /app/g, slugify( options.name ))
+        fs.writeFileSync( file, content, { encoding: 'utf-8' })
+      })
+    })
   } catch ( e ) {
     api.exitLog( `unexpected error in preset: ${e.message}`, 'error' )
   }
---
+++ prompts.js
@@ -0,0 +1,12 @@
+module.exports = [
+  {
+    // basic prompt properties
+    name: 'name',
+    type: 'input',
+    message: 'please add your widget name',
+    default: '',
+    // UI-related prompt properties
+    group: 'Setup modyo',
+    description: 'Set Widget Name',
+  },
+]
